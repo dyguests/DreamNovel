5559682a3fea54e8b5fb3aa4daf17bdf23eb8473
fanhl <fanhonglin@klicen.com>
2018-05-24 09:22:30 +0800

    remove jni temporary

--- app/src/main/java/com/fanhl/dreamnovel/ui/guide/WelcomeActivity.kt
+++ app/src/main/java/com/fanhl/dreamnovel/ui/guide/WelcomeActivity.kt
@@ -13,25 +13,25 @@ class WelcomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
-        // Example of a call to a native method
-        sample_text.text = stringFromJNI()
+//        // Example of a call to a native method
+//        sample_text.text = stringFromJNI()
 
         Handler().postDelayed({
             MainActivity.launch(this@WelcomeActivity)
         }, 2000)
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    external fun stringFromJNI(): String
+//    /**
+//     * A native method that is implemented by the 'native-lib' native library,
+//     * which is packaged with this application.
+//     */
+//    external fun stringFromJNI(): String
 
     companion object {
 
-        // Used to load the 'native-lib' library on application startup.
-        init {
-            System.loadLibrary("native-lib")
-        }
+//        // Used to load the 'native-lib' library on application startup.
+//        init {
+//            System.loadLibrary("native-lib")
+//        }
     }
 }
