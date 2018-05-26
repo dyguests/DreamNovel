55d9450f0ae4b1e855afd25df52c1f0c4d06a6ff
fanhl <dyguests@qq.com>
2018-05-26 23:11:43 +0800

    navi

--- app/src/main/java/com/fanhl/dreamnovel/ui/main/MainActivity.kt
+++ app/src/main/java/com/fanhl/dreamnovel/ui/main/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentStatePagerAdapter
 import com.fanhl.dreamnovel.R
 import com.fanhl.dreamnovel.ui.common.BaseActivity
 import com.fanhl.dreamnovel.ui.read.ReadActivity
@@ -15,29 +17,42 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         assignViews()
+        initData()
     }
 
     private fun assignViews() {
         navigation.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.navigation_home -> {
-                    message.setText(R.string.title_home)
+//                    message.setText(R.string.title_home)
                     return@OnNavigationItemSelectedListener true
                 }
                 R.id.navigation_dashboard -> {
-                    message.setText(R.string.title_dashboard)
+//                    message.setText(R.string.title_dashboard)
                     return@OnNavigationItemSelectedListener true
                 }
                 R.id.navigation_notifications -> {
-                    message.setText(R.string.title_notifications)
+//                    message.setText(R.string.title_notifications)
                     return@OnNavigationItemSelectedListener true
                 }
             }
             false
         })
 
-        btn_read.setOnClickListener {
-            ReadActivity.launch(this@MainActivity)
+//        btn_read.setOnClickListener {
+//            ReadActivity.launch(this@MainActivity)
+//        }
+    }
+
+    private fun initData() {
+        view_pager.adapter = object : FragmentStatePagerAdapter(supportFragmentManager) {
+            override fun getItem(position: Int): Fragment {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun getCount(): Int {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
         }
     }
 
