f2c3b994bc778bdae88950824787520fdb15c297
fanhl <dyguests@qq.com>
2018-05-27 18:21:09 +0800

    wth

--- app/src/main/java/com/fanhl/dreamnovel/ui/main/fragment/DiscoveryFragment.kt
+++ app/src/main/java/com/fanhl/dreamnovel/ui/main/fragment/DiscoveryFragment.kt
@@ -6,6 +6,9 @@ import android.view.ViewGroup
 import com.fanhl.dreamnovel.R
 import com.fanhl.dreamnovel.ui.common.BaseFragment
 
+/**
+ * 发现
+ */
 class DiscoveryFragment : BaseFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) = inflater.inflate(R.layout.fragment_discovery, container, false)!!
 
