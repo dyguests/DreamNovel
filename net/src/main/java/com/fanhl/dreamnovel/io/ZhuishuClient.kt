37898c9f40ae3a9d3ceeddec60c6cdb22df06c69
fanhl <dyguests@qq.com>
2018-05-27 17:38:33 +0800

    ZhuishuService

--- net/src/main/java/com/fanhl/dreamnovel/io/ZhuishuClient.kt
+++ net/src/main/java/com/fanhl/dreamnovel/io/ZhuishuClient.kt
@@ -1,8 +1,10 @@
 package com.fanhl.dreamnovel.io
 
+import io.reactivex.Flowable
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 
 class ZhuishuClient {
     private val retrofit by lazy {
@@ -16,4 +18,9 @@ class ZhuishuClient {
     companion object {
         const val BASE_URL = "http://api.zhuishushenqi.com/"
     }
+}
+
+interface ZhuishuService {
+    @GET("/cats/lv2/statistics")
+    fun getStatistics(): Flowable<Any>
 }
