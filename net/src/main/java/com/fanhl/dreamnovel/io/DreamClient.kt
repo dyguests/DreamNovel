edf203215f1dd1f65d25e6605436e14d7aae8b74
fanhl <dyguests@qq.com>
2018-05-27 17:27:57 +0800

    string

--- net/src/main/java/com/fanhl/dreamnovel/io/DreamClient.kt
+++ net/src/main/java/com/fanhl/dreamnovel/io/DreamClient.kt
@@ -1,11 +1,19 @@
 package com.fanhl.dreamnovel.io
 
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
 class DreamClient {
     private val retrofit by lazy {
-
+        Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
     }
 
     companion object {
-
+        const val BASE_URL = "http://api.zhuishushenqi.com/"
     }
 }
