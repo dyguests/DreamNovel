cf8c062e28e8f61842253825f3256bfad63aee7a
fanhl <dyguests@qq.com>
2018-05-27 17:58:46 +0800

    DiscoveryFragment

--- app/src/main/java/com/fanhl/dreamnovel/ui/main/MainActivity.kt
+++ app/src/main/java/com/fanhl/dreamnovel/ui/main/MainActivity.kt
@@ -36,7 +36,6 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
         view_pager.adapter = adapter
 
