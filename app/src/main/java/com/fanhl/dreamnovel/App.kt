bbf152e11f1d9e716968ae4d851e31f39b13481e
fanhl <dyguests@qq.com>
2018-05-27 17:22:17 +0800

    string

--- app/src/main/java/com/fanhl/dreamnovel/App.kt
+++ app/src/main/java/com/fanhl/dreamnovel/App.kt
@@ -1,5 +1,8 @@
 package com.fanhl.dreamnovel
 
 import android.app.Application
+import com.fanhl.dreamnovel.io.DreamClient
 
-class App : Application()
+class App : Application() {
+    val client by lazy { DreamClient() }
+}
---
+++ net/src/main/java/com/fanhl/dreamnovel/io/DreamClient.kt
@@ -0,0 +1,11 @@
+package com.fanhl.dreamnovel.io
+
+class DreamClient {
+    private val retrofit by lazy {
+
+    }
+
+    companion object {
+
+    }
+}
