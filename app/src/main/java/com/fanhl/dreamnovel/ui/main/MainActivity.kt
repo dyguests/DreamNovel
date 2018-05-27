302a6c16eb8ca68a70aef6582f2c28d1f113a65e
fanhl <dyguests@qq.com>
2018-05-27 17:57:20 +0800

    DiscoveryFragment

--- app/src/main/java/com/fanhl/dreamnovel/ui/main/MainActivity.kt
+++ app/src/main/java/com/fanhl/dreamnovel/ui/main/MainActivity.kt
@@ -2,21 +2,17 @@ package com.fanhl.dreamnovel.ui.main
 
 import android.content.Context
 import android.content.Intent
-import android.support.design.widget.TabLayout
-import android.support.design.widget.Snackbar
-
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentPagerAdapter
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentStatePagerAdapter
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-
 import com.fanhl.dreamnovel.R
 import com.fanhl.dreamnovel.ui.common.BaseActivity
+import com.fanhl.dreamnovel.ui.main.fragment.DiscoveryFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_main.view.*
 
@@ -24,7 +20,18 @@ import kotlinx.android.synthetic.main.fragment_main.view.*
  * 主页
  */
 class MainActivity : BaseActivity() {
-    private val adapter by lazy { SectionsPagerAdapter(supportFragmentManager) }
+    private val adapter by lazy {
+        object : FragmentStatePagerAdapter(supportFragmentManager) {
+            override fun getItem(position: Int) = when (position) {
+                0 -> PlaceholderFragment.newInstance(position + 1)
+                1 -> DiscoveryFragment.newInstance()
+                2 -> PlaceholderFragment.newInstance(position + 1)
+                else -> throw IndexOutOfBoundsException()
+            }
+
+            override fun getCount() = 3
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,20 +62,6 @@ class MainActivity : BaseActivity() {
         }
     }
 
-    inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
-
-        override fun getItem(position: Int): Fragment {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1)
-        }
-
-        override fun getCount(): Int {
-            // Show 3 total pages.
-            return 3
-        }
-    }
-
     /**
      * A placeholder fragment containing a simple view.
      */
@@ -86,7 +79,7 @@ class MainActivity : BaseActivity() {
              * The fragment argument representing the section number for this
              * fragment.
              */
-            private val ARG_SECTION_NUMBER = "section_number"
+            private const val ARG_SECTION_NUMBER = "section_number"
 
             /**
              * Returns a new instance of this fragment for the given section
