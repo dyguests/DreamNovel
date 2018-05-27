edbcc227b6b244e72188b68aa8e9d700464360f9
fanhl <dyguests@qq.com>
2018-05-27 18:02:21 +0800

    主页

--- app/src/main/java/com/fanhl/dreamnovel/ui/main/MainActivity.kt
+++ app/src/main/java/com/fanhl/dreamnovel/ui/main/MainActivity.kt
@@ -5,16 +5,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.TabLayout
-import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentStatePagerAdapter
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.fanhl.dreamnovel.R
 import com.fanhl.dreamnovel.ui.common.BaseActivity
 import com.fanhl.dreamnovel.ui.main.fragment.DiscoveryFragment
+import com.fanhl.dreamnovel.ui.main.fragment.PlaceholderFragment
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_main.view.*
 
 /**
  * 主页
@@ -58,39 +54,6 @@ class MainActivity : BaseActivity() {
         fun launch(context: Context) {
             val intent = Intent(context, MainActivity::class.java)
             context.startActivity(intent)
-        }
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    class PlaceholderFragment : Fragment() {
-
-        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                                  savedInstanceState: Bundle?): View? {
-            val rootView = inflater.inflate(R.layout.fragment_main, container, false)
-            rootView.section_label.text = getString(R.string.section_format, arguments?.getInt(ARG_SECTION_NUMBER))
-            return rootView
-        }
-
-        companion object {
-            /**
-             * The fragment argument representing the section number for this
-             * fragment.
-             */
-            private const val ARG_SECTION_NUMBER = "section_number"
-
-            /**
-             * Returns a new instance of this fragment for the given section
-             * number.
-             */
-            fun newInstance(sectionNumber: Int): PlaceholderFragment {
-                val fragment = PlaceholderFragment()
-                val args = Bundle()
-                args.putInt(ARG_SECTION_NUMBER, sectionNumber)
-                fragment.arguments = args
-                return fragment
-            }
         }
     }
 }
