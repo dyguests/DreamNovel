ff93bc8e962bb4e1afd26a458c0a14c58e586d00
fanhl <dyguests@qq.com>
2018-12-23 10:38:29 +0800

    test

---
+++ app/src/main/java/com/fanhl/dreamnovel/ui/test/PageTestActivity.kt
@@ -0,0 +1,13 @@
+package com.fanhl.dreamnovel.ui.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fanhl.dreamnovel.R
+
+class PageTestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page_test)
+    }
+}
