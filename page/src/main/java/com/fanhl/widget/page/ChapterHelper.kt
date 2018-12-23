cb4ab82cf454741fb5ed8782514cac70318d79f8
fanhl <dyguests@qq.com>
2018-12-23 15:59:18 +0800

    contentPaint

---
+++ page/src/main/java/com/fanhl/widget/page/ChapterHelper.kt
@@ -0,0 +1,10 @@
+package com.fanhl.widget.page
+
+object ChapterHelper {
+    /**
+     * 对章节进行分页处理
+     */
+    fun pagination(chapter: Chapter, width: Int, height: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
--- page/src/main/java/com/fanhl/widget/page/ChapterView.kt
+++ page/src/main/java/com/fanhl/widget/page/ChapterView.kt
@@ -3,6 +3,7 @@ package com.fanhl.widget.page
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
+import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.View
 import androidx.annotation.Nullable
@@ -13,6 +14,25 @@ import androidx.annotation.Nullable
 class ChapterView(
     context: Context, @Nullable attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+
+    /**
+     *  绘制小说内容的画笔
+     */
+    private val contentPaint: TextPaint = TextPaint()
+
+    var chapter: Chapter? = null
+        set(value) {
+            if (field == value) {
+                return
+            }
+
+            field = value
+
+        }
+
+    init {
+
+    }
 
     override fun onDraw(canvas: Canvas) {
         canvas.drawColor(Color.RED)
