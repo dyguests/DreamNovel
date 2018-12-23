cb15e3c2b80f43dac859eec8d22ffeab0b264317
fanhl <dyguests@qq.com>
2018-12-23 10:03:23 +0800

    refactor

--- app/src/main/java/com/fanhl/dreamnovel/App.kt
+++
@@ -1,8 +0,0 @@
-package com.fanhl.dreamnovel
-
-import android.app.Application
-import com.fanhl.dreamnovel.io.ZhuishuClient
-
-class App : Application() {
-    val zhuishuClient by lazy { ZhuishuClient() }
-}
--- app/src/main/java/com/fanhl/dreamnovel/ui/common/BaseActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.fanhl.dreamnovel.ui.common
-
-import android.support.v7.app.AppCompatActivity
-
-abstract class BaseActivity : AppCompatActivity() {
-
-}
