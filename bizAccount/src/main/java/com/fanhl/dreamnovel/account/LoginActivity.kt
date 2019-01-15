bc8226109537e69ce2e77cbf1d491d981c648b69
fanhl <fanhonglin@klicen.com>
2019-01-15 18:17:46 +0800

    添加跳转逻辑

--- bizAccount/src/main/java/com/fanhl/dreamnovel/account/LoginActivity.kt
+++ bizAccount/src/main/java/com/fanhl/dreamnovel/account/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.fanhl.dreamnovel.account
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import com.alibaba.android.arouter.launcher.ARouter
 import com.fanhl.dreamnovel.base.BaseActivity
 import com.jaeger.library.StatusBarUtil
 import kotlinx.android.synthetic.main.activity_login.*
@@ -16,7 +17,8 @@ class LoginActivity : BaseActivity() {
         setContentView(R.layout.activity_login)
 
         btn_login.onClick {
-
+            // 1. 应用内简单的跳转(通过URL跳转在'进阶用法'中)
+            ARouter.getInstance().build("/home/main").navigation()
         }
     }
 
