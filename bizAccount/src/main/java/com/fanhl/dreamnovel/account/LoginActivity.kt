9cf942341503017cf400b1ecb3c5e781468ea141
fanhl <fanhonglin@klicen.com>
2019-01-15 16:04:44 +0800

    透明 状态栏 ok

--- bizAccount/src/main/java/com/fanhl/dreamnovel/account/LoginActivity.kt
+++ bizAccount/src/main/java/com/fanhl/dreamnovel/account/LoginActivity.kt
@@ -17,7 +17,6 @@ class LoginActivity : BaseActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         }
-//        StatusBarUtil.setTranslucent(this)
         setContentView(R.layout.activity_login)
     }
 
