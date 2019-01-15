ea1ec5b4c845289fd997eed26987aad82a281d9b
fanhl <fanhonglin@klicen.com>
2019-01-15 10:00:23 +0800

    base

--- app/src/main/java/com/fanhl/dreamnovel/ui/main/MainActivity.kt
+++ app/src/main/java/com/fanhl/dreamnovel/ui/main/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.fanhl.dreamnovel.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.fanhl.dreamnovel.R
 
 class MainActivity : AppCompatActivity() {
