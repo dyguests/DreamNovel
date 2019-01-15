25320a2317a6975e6b2c23e15694e5725620265b
fanhl <fanhonglin@klicen.com>
2019-01-15 10:58:40 +0800

    launch

--- app/src/main/java/com/fanhl/dreamnovel/ui/entrance/SplashActivity.kt
+++ app/src/main/java/com/fanhl/dreamnovel/ui/entrance/SplashActivity.kt
@@ -2,12 +2,19 @@ package com.fanhl.dreamnovel.ui.entrance
 
 import android.os.Bundle
 import com.fanhl.dreamnovel.R
+import com.fanhl.dreamnovel.account.LoginActivity
 import com.fanhl.dreamnovel.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        tv_name.postDelayed({
+            LoginActivity.launch(this@SplashActivity)
+            finish()
+        }, 2000)
     }
 }
