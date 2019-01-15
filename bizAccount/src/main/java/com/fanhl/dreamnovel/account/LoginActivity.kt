4f41e9bb77d696e1815bdf0fede2b598fefab316
fanhl <fanhonglin@klicen.com>
2019-01-15 15:00:41 +0800

    back

--- bizAccount/src/main/java/com/fanhl/dreamnovel/account/LoginActivity.kt
+++ bizAccount/src/main/java/com/fanhl/dreamnovel/account/LoginActivity.kt
@@ -2,13 +2,19 @@ package com.fanhl.dreamnovel.account
 
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import com.fanhl.dreamnovel.base.BaseActivity
 
 class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+//        StatusBarUtil.setTranslucent(this)
         setContentView(R.layout.activity_login)
     }
 
