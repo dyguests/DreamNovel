9c3a35d0db04c40bbfbaa20526a26406d5374045
fanhl <fanhonglin@klicen.com>
2019-01-15 18:21:44 +0800

    跳转不了

--- app/src/main/java/com/fanhl/dreamnovel/App.kt
+++ app/src/main/java/com/fanhl/dreamnovel/App.kt
@@ -12,10 +12,10 @@ class App : BaseApp() {
         }
 //        // 这两行必须写在init之前，否则这些配置在init过程中将无效
 //        if (isDebug()) {
-//            // 打印日志
-//            ARouter.openLog()
-//            // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
-//            ARouter.openDebug()
+            // 打印日志
+            ARouter.openLog()
+            // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+            ARouter.openDebug()
 //        }
         // 尽可能早，推荐在Application中初始化
         ARouter.init(this)
