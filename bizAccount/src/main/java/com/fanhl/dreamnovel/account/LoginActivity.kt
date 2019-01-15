94702e82238640b39c1251baf8fdc9a4ab95316f
fanhl <fanhonglin@klicen.com>
2019-01-15 15:28:32 +0800

    login_top

--- bizAccount/src/main/java/com/fanhl/dreamnovel/account/LoginActivity.kt
+++ bizAccount/src/main/java/com/fanhl/dreamnovel/account/LoginActivity.kt
@@ -2,19 +2,18 @@ package com.fanhl.dreamnovel.account
 
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.view.View
 import com.fanhl.dreamnovel.base.BaseActivity
+import com.jaeger.library.StatusBarUtil
 
 class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-        }
-//        StatusBarUtil.setTranslucent(this)
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+//        }
+        StatusBarUtil.setTranslucent(this)
         setContentView(R.layout.activity_login)
     }
 
