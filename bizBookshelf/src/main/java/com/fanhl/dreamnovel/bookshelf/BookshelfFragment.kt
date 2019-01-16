681bfa30bfaeb3300909bce24e213f9a99951a10
fanhl <fanhonglin@klicen.com>
2019-01-16 17:57:49 +0800

    book

--- bizBookshelf/src/main/java/com/fanhl/dreamnovel/bookshelf/BookshelfFragment.kt
+++ bizBookshelf/src/main/java/com/fanhl/dreamnovel/bookshelf/BookshelfFragment.kt
@@ -5,7 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.BaseViewHolder
 import kotlinx.android.synthetic.main.fragment_bookshelf.*
 
 /**
@@ -14,7 +15,6 @@ import kotlinx.android.synthetic.main.fragment_bookshelf.*
  * @author fanhl
  */
 class BookshelfFragment : Fragment() {
-//    private var listener: OnFragmentInteractionListener? = null
 
     private val adapter by lazy { BookshelfAdapter() }
 
@@ -23,62 +23,19 @@ class BookshelfFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         recycler_view.adapter = adapter
+
+        adapter.setNewData(List(10) { Book() })
     }
-    //    // TODO: Rename method, update argument and hook method into UI event
-//    fun onButtonPressed(uri: Uri) {
-//        listener?.onFragmentInteraction(uri)
-//    }
-
-//    override fun onAttach(context: Context) {
-//        super.onAttach(context)
-//        if (context is OnFragmentInteractionListener) {
-//            listener = context
-//        } else {
-//            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
-//        }
-//    }
-//
-//    override fun onDetach() {
-//        super.onDetach()
-//        listener = null
-//    }
-
-//    /**
-//     * This interface must be implemented by activities that contain this
-//     * fragment to allow an interaction in this fragment to be communicated
-//     * to the activity and potentially other fragments contained in that
-//     * activity.
-//     *
-//     *
-//     * See the Android Training lesson [Communicating with Other Fragments]
-//     * (http://developer.android.com/training/basics/fragments/communicating.html)
-//     * for more information.
-//     */
-//    interface OnFragmentInteractionListener {
-//        // TODO: Update argument type and name
-//        fun onFragmentInteraction(uri: Uri)
-//    }
 
     companion object {
-        fun newInstance() = BookshelfFragment().apply {
-            arguments = Bundle().apply {
-                //                    putString(ARG_PARAM2, param2)
-            }
-        }
+        fun newInstance() = BookshelfFragment()
     }
 }
 
-class BookshelfAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+class BookshelfAdapter : BaseQuickAdapter<Book, BaseViewHolder>(R.layout.item_bookshelf_diary) {
+    override fun convert(helper: BaseViewHolder?, item: Book?) {
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
 }
+
+class Book
