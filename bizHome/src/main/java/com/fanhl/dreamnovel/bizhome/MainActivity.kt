7e828b703f3fbf093f697ed4b67bbee877285e09
fanhl <fanhonglin@klicen.com>
2019-01-16 15:29:13 +0800

    view_pager 2

--- bizHome/src/main/java/com/fanhl/dreamnovel/bizhome/MainActivity.kt
+++ bizHome/src/main/java/com/fanhl/dreamnovel/bizhome/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.fanhl.dreamnovel.bizhome
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentStatePagerAdapter
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.fanhl.dreamnovel.base.ARouters
 import com.fanhl.dreamnovel.base.BaseActivity
@@ -9,10 +11,24 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 @Route(path = ARouters.Home.MAIN)
 class MainActivity : BaseActivity() {
+    private val adapter by lazy {
+        object : FragmentStatePagerAdapter(supportFragmentManager) {
+            override fun getItem(position: Int): Fragment {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun getCount(): Int {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        view_pager.adapter = adapter
 
         navigation.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
             when (item.itemId) {
