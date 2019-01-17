b5a4ab823803429f0bc0dcc675e9932dbb1bf320
fanhl <fanhonglin@klicen.com>
2019-01-17 14:46:57 +0800

    好像通了

--- app/src/main/java/com/fanhl/dreamnovel/database/AppDatabase.kt
+++ app/src/main/java/com/fanhl/dreamnovel/database/AppDatabase.kt
@@ -1,12 +1,12 @@
 package com.fanhl.dreamnovel.database
 
 import android.content.Context
-import android.util.Log
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.fanhl.dreamnovel.bizwriting.database.dao.ArticleDao
 import com.fanhl.dreamnovel.bizwriting.database.entity.Article
+import com.fanhl.dreamnovel.ui.entrance.SplashActivity
 
 @Database(entities = [Article::class], version = 1)
 abstract class AppDatabase : RoomDatabase(), IAppDatabase {
@@ -14,7 +14,10 @@ abstract class AppDatabase : RoomDatabase(), IAppDatabase {
     abstract fun articleDao(): ArticleDao
 
     override fun <T> get(clazz: Class<T>): T? {
-        if (clazz.isAssignableFrom(ArticleDao::class.java)) {
+        if (clazz.isAssignableFrom(SplashActivity::class.java)) {
+            //FIXME just for test
+            return null
+        } else if (clazz.isAssignableFrom(ArticleDao::class.java)) {
             return articleDao() as? T
         }
         //        if (T instanceof ArticleDao) {
@@ -31,7 +34,7 @@ abstract class AppDatabase : RoomDatabase(), IAppDatabase {
 
         fun init(context: Context) {
 //            if (IAppDatabase.INSTANCE == null) {
-                IAppDatabase.INSTANCE = Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "dream_novel").build()
+            IAppDatabase.INSTANCE = Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "dream_novel").build()
 //            } else {
 //                Log.d(TAG, "AppDatabase already inited")
 //            }
