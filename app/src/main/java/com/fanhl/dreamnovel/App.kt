1242fe833fd003502b0f1196a8c944cbfd50d9ef
fanhl <fanhonglin@klicen.com>
2019-01-17 09:30:54 +0800

    initARouter

--- app/src/main/java/com/fanhl/dreamnovel/App.kt
+++ app/src/main/java/com/fanhl/dreamnovel/App.kt
@@ -7,17 +7,20 @@ import org.jetbrains.anko.doAsync
 class App : BaseApp() {
     override fun onCreate() {
         super.onCreate()
-        doAsync {
+        initARouter()
+    }
 
-        }
-//        // 这两行必须写在init之前，否则这些配置在init过程中将无效
-//        if (isDebug()) {
+    private fun initARouter() {
+        doAsync {
+            //        // 这两行必须写在init之前，否则这些配置在init过程中将无效
+            //        if (isDebug()) {
             // 打印日志
             ARouter.openLog()
             // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
             ARouter.openDebug()
-//        }
-        // 尽可能早，推荐在Application中初始化
-        ARouter.init(this)
+            //        }
+            // 尽可能早，推荐在Application中初始化
+            ARouter.init(this@App)
+        }
     }
 }
