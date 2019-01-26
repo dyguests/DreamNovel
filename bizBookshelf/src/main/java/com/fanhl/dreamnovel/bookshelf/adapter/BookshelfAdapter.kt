1973b141917409aa319024747d84fefa2ae419fd
fanhl <dyguests@qq.com>
2019-01-26 14:37:37 +0800

    title

--- bizBookshelf/src/main/java/com/fanhl/dreamnovel/bookshelf/adapter/BookshelfAdapter.kt
+++ bizBookshelf/src/main/java/com/fanhl/dreamnovel/bookshelf/adapter/BookshelfAdapter.kt
@@ -1,5 +1,6 @@
 package com.fanhl.dreamnovel.bookshelf.adapter
 
+import android.view.View
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.fanhl.dreamnovel.bookshelf.R
@@ -9,7 +10,13 @@ import kotlinx.android.synthetic.main.item_bookshelf_diary.view.*
 class BookshelfAdapter : BaseQuickAdapter<Article, BaseViewHolder>(R.layout.item_bookshelf_diary) {
     override fun convert(helper: BaseViewHolder?, item: Article?) {
         helper?.itemView?.apply {
-            btn_add.text = item?.content
+            item?.title?.takeIf { it.isNotEmpty() }?.let {
+                tv_title.visibility = View.VISIBLE
+                tv_title.text = it
+            } ?: let {
+                tv_title.visibility = View.GONE
+            }
+            tv_content.text = item?.content
         }
     }
 }
